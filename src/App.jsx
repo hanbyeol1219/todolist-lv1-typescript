c991e3504516583d3e38621c490e07f5a00b5294
Lee Hanbyeol <leehanbyeol971219@gmail.com>
2023-06-15 11:28:45 +0900

    App.jsx 파일 내에서 컴포넌트 분리

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,6 @@ import "./App.css";
 const App = () => {
   const [todos, setTodos] = useState([{}]);
 
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-
-  const todoTitleChangeHandler = (event) => {
-    setTitle(event.target.value);
-  };
-  const todoContentChangeHandler = (event) => {
-    setContent(event.target.value);
-  };
-
-  const toDoAddButtonHandler = (event) => {
-    event.preventDefault();
-
-    const newTodo = {
-      id: uuidv4(),
-      title,
-      content,
-      isDone: false,
-    };
-
-    setTodos([...todos, newTodo]);
-    setTitle("");
-    setContent("");
-  };
-
   const toDoDeleteButtonHandler = (id) => {
     const deleteTodos = todos.filter((todo) => todo.id !== id);
     setTodos(deleteTodos);
@@ -62,25 +37,7 @@ const App = () => {
         <h1>My Todo List</h1>
         <span>React</span>
       </header>
-      <div>
-        <form id="form">
-          <div id="form-input-container">
-            <span>제목</span>
-            <input
-              type="text"
-              value={title}
-              onChange={todoTitleChangeHandler}
-            />
-            <span>내용</span>
-            <input
-              type="text"
-              value={content}
-              onChange={todoContentChangeHandler}
-            />
-          </div>
-          <button onClick={toDoAddButtonHandler}>추가하기</button>
-        </form>
-      </div>
+      <AddTodo todos={todos} setTodos={setTodos} />
       <div className="todo-list-container">
         <h2 className="working-title">Working..🔥</h2>
         <ul className="todo-list">
@@ -90,21 +47,11 @@ const App = () => {
             })
             .map((item) => {
               return (
-                <li key={item.id} className="todo-box">
-                  <div className="todo-content-wrap">
-                    <span className="todo-title">{item.title}</span>
-                    <br />
-                    <span className="todo-content">{item.content}</span>
-                    <div className="button-wrap">
-                      <button onClick={() => toDoDeleteButtonHandler(item.id)}>
-                        삭제하기
-                      </button>
-                      <button onClick={() => toDoDoneButtonHandler(item.id)}>
-                        완료
-                      </button>
-                    </div>
-                  </div>
-                </li>
+                <Working
+                  item={item}
+                  toDoDeleteButtonHandler={toDoDeleteButtonHandler}
+                  toDoDoneButtonHandler={toDoDoneButtonHandler}
+                />
               );
             })}
         </ul>
@@ -118,26 +65,96 @@ const App = () => {
             })
             .map((item) => {
               return (
-                <li key={item.id} className="todo-box">
-                  <div className="todo-content-wrap">
-                    <span className="todo-title">{item.title}</span>
-                    <br />
-                    <span className="todo-content">{item.content}</span>
-                    <div className="button-wrap">
-                      <button onClick={() => toDoDeleteButtonHandler(item.id)}>
-                        삭제하기
-                      </button>
-                      <button onClick={() => toDoCancleButtonHandler(item.id)}>
-                        취소
-                      </button>
-                    </div>
-                  </div>
-                </li>
+                <Done
+                  item={item}
+                  toDoDeleteButtonHandler={toDoDeleteButtonHandler}
+                  toDoCancleButtonHandler={toDoCancleButtonHandler}
+                />
               );
             })}
         </ul>
       </div>
     </div>
+  );
+};
+
+const AddTodo = ({ todos, setTodos }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const todoTitleChangeHandler = (event) => {
+    setTitle(event.target.value);
+  };
+  const todoContentChangeHandler = (event) => {
+    setContent(event.target.value);
+  };
+
+  const toDoAddButtonHandler = (event) => {
+    event.preventDefault();
+
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      content,
+      isDone: false,
+    };
+
+    setTodos([...todos, newTodo]);
+    setTitle("");
+    setContent("");
+  };
+  return (
+    <div>
+      <form id="form">
+        <div id="form-input-container">
+          <span>제목</span>
+          <input type="text" value={title} onChange={todoTitleChangeHandler} />
+          <span>내용</span>
+          <input
+            type="text"
+            value={content}
+            onChange={todoContentChangeHandler}
+          />
+        </div>
+        <button onClick={toDoAddButtonHandler}>추가하기</button>
+      </form>
+    </div>
+  );
+};
+
+const Working = ({ item, toDoDeleteButtonHandler, toDoDoneButtonHandler }) => {
+  return (
+    <li key={item.id} className="todo-box">
+      <div className="todo-content-wrap">
+        <span className="todo-title">{item.title}</span>
+        <br />
+        <span className="todo-content">{item.content}</span>
+        <div className="button-wrap">
+          <button onClick={() => toDoDeleteButtonHandler(item.id)}>
+            삭제하기
+          </button>
+          <button onClick={() => toDoDoneButtonHandler(item.id)}>완료</button>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+const Done = ({ item, toDoDeleteButtonHandler, toDoCancleButtonHandler }) => {
+  return (
+    <li key={item.id} className="todo-box">
+      <div className="todo-content-wrap">
+        <span className="todo-title">{item.title}</span>
+        <br />
+        <span className="todo-content">{item.content}</span>
+        <div className="button-wrap">
+          <button onClick={() => toDoDeleteButtonHandler(item.id)}>
+            삭제하기
+          </button>
+          <button onClick={() => toDoCancleButtonHandler(item.id)}>취소</button>
+        </div>
+      </div>
+    </li>
   );
 };
 
